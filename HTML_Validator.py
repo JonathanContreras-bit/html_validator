37bf82832d95fd2b1438b28f5b28b2fbe0f2fcdf
Jonathan Contreras <jonathan.contreras2@pomona.edu>
2021-02-11 20:15:07 -0800

    define _extract_tags and validate_html functions

--- HTML_Validator.py
+++ HTML_Validator.py
@@ -10,6 +10,20 @@ def validate_html(html):
     >>> validate_html('<strong>example')
     False
     '''
+    # enforces proper order / formatting
+    stack = []
+    if _extract_tags(html):
+        for tag in _extract_tags(html):
+            if tag[1] != '/':
+                stack.append(tag)
+            else:
+                if len(stack) == 0:
+                    return False
+                if (stack[-1][1] != '/' and tag[2:] == stack[-1][1:]):
+                    stack.pop()
+        return len(stack) == 0
+    else:
+        return html == ''
 
     # HINT:
     # use the _extract_tags function below to generate a list of html tags without any extra text;
@@ -29,3 +43,18 @@ def _extract_tags(html):
     >>> _extract_tags('Python <strong>rocks</strong>!')
     ['<strong>', '</strong>']
     '''
+    tags = []
+    for i in range(len(html)):
+        count = 1
+        if html[i] == '<':
+            while ((i + count) < len(html)) and (html[i + count] != '>'):
+                count += 1
+            if (i + count) >= len(html):
+                return tags
+            else:
+                string = html[i:i + count + 1]
+                if ' ' in string:
+                    space_i = string.index(' ')
+                    string = string[:space_i] + string[-1]
+                tags.append(string)
+    return tags
